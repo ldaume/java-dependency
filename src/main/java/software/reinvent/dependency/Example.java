2c0ed536782016e048601b1c715338132a071b90
Leonard Daume <lenny@reinvent.software>
2017-01-05 21:53:32 +0100

    template version 0.1.0

---
+++ src/main/java/software/reinvent/dependency/Example.java
@@ -0,0 +1,14 @@
+package software.reinvent.dependency;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created on 05.01.2017.
+ *
+ * @author <a href="mailto:lenny@reinvent.software">Leonard Daume</a>
+ */
+public class Example {
+    public static String helloWorld(String name) {
+        return StringUtils.join("Hello ", name, "!");
+    }
+}
---
+++ src/test/java/software/reinvent/dependency/ExampleTest.java
@@ -0,0 +1,17 @@
+package software.reinvent.dependency;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+/**
+ * Created on 05.01.2017.
+ *
+ * @author <a href="mailto:lenny@reinvent.software">Leonard Daume</a>
+ */
+public class ExampleTest {
+    @Test
+    public void testHelloWorld() throws Exception {
+        Assertions.assertThat(Example.helloWorld("Lenny")).isEqualTo("Hello Lenny!");
+
+    }
+}
